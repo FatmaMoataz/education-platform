17db9efb341458178747a71625e3a12843b1907c
mahmoudAliaboElhassan <ml6893254@gmail.com>
2025-07-02 18:32:42 +0300

    sign up login verify

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { motion } from "motion/react"
-import Layout from './components/Layout/Layout'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { motion } from "motion/react";
+import Layout from "./components/Layout/Layout";
+import { Provider } from "react-redux";
+import { store } from "./state/store";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-  <motion.div animate={{ x: 50 }} class="text-3xl font-bold underline">
-    <Layout/>
-  </motion.div>
-
+      <motion.div animate={{ x: 50 }} class="text-3xl font-bold underline">
+        <Provider store={store}>
+          <Layout />
+        </Provider>
+      </motion.div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/state/act/actAuth.js
@@ -0,0 +1,48 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { eduAPI } from "../../utils/axiosInstance";
+
+export const signUp = createAsyncThunk(
+  "authSlice/signUp",
+  async (userData, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+
+    try {
+      const res = await eduAPI.post("auth/signup", userData);
+      console.log("from slice res is");
+      console.log(res);
+      return res;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+export const logIn = createAsyncThunk(
+  "authSlice/logIn",
+  async (userData, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+
+    try {
+      const res = await eduAPI.post("auth/login", userData);
+      console.log("from slice res is");
+      console.log(res);
+      return res.data;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+export const verify = createAsyncThunk(
+  "authSlice/verify",
+  async ({ token }, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+
+    try {
+      const res = await eduAPI.post(`auth/verify/${token}`, userData);
+      console.log("from slice res is");
+      console.log(res);
+      return res.data;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
