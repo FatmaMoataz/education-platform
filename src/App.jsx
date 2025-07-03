b2563ce3c8c1e130b16910ecd187f8c494509fc3
FatmaMoataz <fatmamoataz65@gmail.com>
2025-07-03 09:08:19 +0200

    Finished login/register feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { motion } from "motion/react";
-import Layout from "./components/Layout/Layout";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./state/store";
+import Layout from "./components/Layout/Layout";
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
 
 function App() {
+  const token = localStorage.getItem("token");
 
   return (
-    <>
-      <motion.div animate={{ x: 50 }} class="text-3xl font-bold underline">
-        <Provider store={store}>
-          <Layout />
-        </Provider>
-      </motion.div>
-    </>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          {/* Root route: Redirect to /signup if no token, else /home */}
+          <Route
+            path="/"
+            element={
+              token ? <Navigate to="/home" replace /> : <Navigate to="/signup" replace />
+            }
+          />
+
+          {/* Public routes (no token required) */}
+          <Route path="/signup" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+
+          {/* Protected route (requires token) */}
+          <Route
+            path="/home"
+            element={token ? <Layout /> : <Navigate to="/login" replace />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,26 @@
-import React from "react";
-import { useDispatch } from "react-redux";
 import { signUp } from "../../state/act/actAuth";
 
 export default function Navbar() {
-  const dispatch = useDispatch();
+ 
   return (
-    <button
-      onClick={() =>
-        dispatch(
-          signUp({
-            fullName: "mo ashraf ",
-            email: "mahmoudaliefullstack@essgmail.com",
-            password: "My987654321@",
-            cpassword: "My987654321@",
-            phoneNumber: "01165891463",
-            classLevel: "Grade 1 Secondary",
-          })
-        )
-      }
-    >
-      Navbar
-    </button>
+    <>
+    <h1>Navbar</h1>
+    </>
+    // <button
+    //   onClick={() =>
+    //     dispatch(
+    //       signUp({
+    //         fullName: "mo ashraf ",
+    //         email: "mahmoudaliefullstack@essgmail.com",
+    //         password: "My987654321@",
+    //         cpassword: "My987654321@",
+    //         phoneNumber: "01165891463",
+    //         classLevel: "Grade 1 Secondary",
+    //       })
+    //     )
+    //   }
+    // >
+    //   Navbar
+    // </button>
   );
 }
